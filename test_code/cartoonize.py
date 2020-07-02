64a6b6fe60b2eafc7a0a4a6b7603dfbd4a485497
Ahsen <ahsen.khaliq@gmail.com>
2020-07-02 00:14:19 -0400

    cv2 read

--- test_code/cartoonize.py
+++ test_code/cartoonize.py
@@ -41,7 +41,7 @@ def cartoonize(input_image, model_path):
     print("Model Loaded")
 
     print("Processing Input")
-    image = input_image
+    image = cv2.imread(input_image)
     image = resize_crop(image)
     # batch_image = image.astype(np.float32)/127.5 - 1
     batch_image = np.array(image)
